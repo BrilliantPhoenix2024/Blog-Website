f87d4ca64ffb3ab9f28bddd7d9accf5c447ad586
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-06-11 16:17:43 +0330

    templates in root DIR added

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home_page_view(request):
     return render(request, 'home.html')
 
+
 def about_page_view(request):
     return render(request, 'pages/about.html')
+
 
 def contact_page_view(request):
     return HttpResponse('contact_page_view')
