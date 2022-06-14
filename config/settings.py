c041f71cc0240cda8c90ac002d902cf5338e9f96
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-06-14 10:48:12 +0330

    replace home.html to pages/template/pages/ & accounts app create & confige in settings.py

--- config/settings.py
+++ config/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-
+    # my local apps
     'pages',
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- pages/views.py
+++ pages/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 
 def home_page_view(request):
-    return render(request, 'home.html')
+    return render(request, 'pages/home.html')
 
 
 def about_page_view(request):
