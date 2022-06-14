56fc4aba43c38a71f9bb140f3239c26da0b56bc9
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-06-14 20:16:14 +0330

    form login & home url edited

--- pages/urls.py
+++ pages/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home_page_view, name='home'),
+    path('accounts/login', views.home_page_view, name='home'),
     path('about/', views.about_page_view, name='about'),
     path('contact/', views.contact_page_view, name='contact'),
     path('design/', views.design_page_view, name='design'),
     path('services/', views.services_page_view, name='services'),
 ]
+
