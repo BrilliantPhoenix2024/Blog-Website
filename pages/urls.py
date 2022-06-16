962816d7e95a6d570e2f045a4e13d727d6d1019f
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-06-16 20:18:01 +0330

    home & login replaced in diffrent pages---login authenticated system configed

--- pages/urls.py
+++ pages/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('accounts/login', views.home_page_view, name='home'),
+    path('', views.home_page_view, name='home'),
     path('about/', views.about_page_view, name='about'),
     path('contact/', views.contact_page_view, name='contact'),
     path('design/', views.design_page_view, name='design'),
