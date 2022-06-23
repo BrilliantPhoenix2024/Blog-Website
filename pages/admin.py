6289a9aef8f5589b5e4b03891b810cbfce2a48e6
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-06-23 20:41:59 +0330

    admin.py modidied

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import My_design
 
-# Register your models here.
+@admin.register(My_design)
+class My_designAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'datetime_modified', )
+
+
+# admin.site.register(My_design, My_designAdmin)
+
--- pages/models.py
+++ pages/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class my_design(models.Model):
+class My_design(models.Model):
     STATUS_CHOICES = (
         ('pub', 'Published'),
         ('drf', 'Draft'),
@@ -12,6 +12,10 @@ class my_design(models.Model):
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
     status = models.CharField(choices=STATUS_CHOICES, max_length=3)
+
+    def __str__(self):
+        return self.title
+
 
 
 
