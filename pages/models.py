79f4926536f2bfeb40e456de521d9308ab74ae67
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-06-23 18:00:23 +0330

    model.py for designs created

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class my_design(models.Model):
+    STATUS_CHOICES = (
+        ('pub', 'Published'),
+        ('drf', 'Draft'),
+    )
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=3)
+
+
+
+
