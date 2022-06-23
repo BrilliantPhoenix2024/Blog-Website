949e1fa8e659be722f910fc5ec944623c6c9afee
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-06-23 20:49:28 +0330

    admin.py ordering by -status

--- pages/admin.py
+++ pages/admin.py
@@ -4,6 +4,7 @@ from .models import My_design
 @admin.register(My_design)
 class My_designAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'datetime_modified', )
+    ordering = ('-status', )
 
 
 # admin.site.register(My_design, My_designAdmin)
