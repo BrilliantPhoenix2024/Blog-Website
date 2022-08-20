6f37c03288deae2450dae453ac984ea21473dd60
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-20 20:17:35 +0330

    Edit settings.py for installing crispy-forms Package

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third-Party APPs
+    'crispy_forms',
+
     # my local apps
     'pages',
     'accounts',
