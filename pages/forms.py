530723ba084c5b45905e8bca603accd3d03332b6
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-20 20:03:32 +0330

    Create 'ContactForm' for contact.html in forms.py

--- pages/forms.py
+++ pages/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import My_design
+from .models import My_design, Contact
+
 
 class DesignForm(forms.ModelForm):
     class Meta:
         model = My_design
         fields = ['title', 'description', 'author', 'status']
 
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'msg']
