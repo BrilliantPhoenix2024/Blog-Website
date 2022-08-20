5cf2d87613b96ea54be170c7327c66cf614e0a2d
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-20 18:55:32 +0330

    Register 'Contact' Model in admin.py

--- pages/admin.py
+++ pages/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import My_design
+from .models import My_design, Contact
 
 
 @admin.register(My_design)
-class My_designAdmin(admin.ModelAdmin):
+class DesignAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'datetime_modified', )
     ordering = ('-status', )
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'msg', )
 
 
 # admin.site.register(My_design, My_designAdmin)
